649cd1fa221d402cf84879bd69614c9f622e05b4
artur-bertash <arturbertash88@gmail.com>
2025-10-09 09:20:39 -0600

    added Home/LearnMore/Videos/parsed video desc and img url

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,19 @@
 import Home from "./routes/Home"
-
+import Videos from "./routes/Videos"
+import LearnMore from "./routes/LearnMore"
 
 const routes = [
     {
         path: "/",
         element: <Home/>
+    }, 
+    {
+        path: "/videos",
+        element: <Videos/>
+    },
+    {
+        path: "/learn-more",
+        element: <LearnMore/>
     }
 ]
 
--- src/routes/Videos.jsx
+++ src/routes/Videos.jsx
@@ -1,8 +1,14 @@
+import episodes  from "../data/frenchInActionEpisodes"
+import "../styles/Videos.css"
+import Video from "../components/Video"
+
 function Videos() {
     return (
-        <>
-        
-        </>
+        <div className="videos">
+        {episodes.map((episode) => (
+            <Video key={episode.id} episode={episode} />
+        ))}
+        </div>
     )
 }
 export default Videos
