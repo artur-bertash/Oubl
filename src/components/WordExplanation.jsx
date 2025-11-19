2f81b8df0f8f4e2bbd0166a30f7cef437b996825
artur-bertash <arturbertash88@gmail.com>
2025-11-18 18:18:55 -0700

    back

--- src/components/WordExplanation.jsx
+++ src/components/WordExplanation.jsx
@@ -4,58 +4,20 @@ function WordExplanation({word, previousCues, nextCues, currCue}) {
     const [response, setResponse] = useState(null);
     const [loading, setLoaoding] = useState(true);
 
-    const key = import.meta.env.VITE_OPEN_ROUTER_API_KEY;
+    
 
     useEffect(() => {
-        async function fetchExplanation() {
-            setLoaoding(true);
-            const res = await fetch('https://openrouter.ai/api/v1/chat/completions', {
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${key}`,
-                    'HTTP-Referer': '<YOUR_SITE_URL>', // Optional. Site URL for rankings on openrouter.ai.
-                    'X-Title': '<YOUR_SITE_NAME>', // Optional. Site title for rankings on openrouter.ai.
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    model: 'qwen/qwen3-30b-a3b:free',
-                    messages: [
-                    {
-                        role: 'user',
-                        content: `
-                Explain the target French word in context.
-                Give 1–3 short, natural sentences in English, not labels or bullet points. Do not overword, but be clear. 
-                Do NOT use headings like “Form:” or “Meaning:”.
-                Explain:
-                what it means in this specific sentence
-                Keep it simple, natural and short.
+        async function getExplanation(word, prev, next) {
+            const res = await fetch("/api/explain", {
+                method: "POST",
+                body: JSON.stringify({ word, previousCues, nextCues, currCue }),
+            });
 
-                Format:
-                <short explanation written in normal sentences>
+            return await res.json();
+            }
 
-                Input:
-                Word: ${word}
-                Previous: ${previousCues}
-                Current: ${currCue}
-                Next: ${nextCues}
-                        `,
-                    },
-                    ],
-                }),
-                });
-                const data = await res.json();
-                console.log(data);
-                console.log(`
-                    Word: ${word}
-                    Previous: ${previousCues}
-                    Current: ${currCue}
-                    Next: ${nextCues}
-                    `)
-                setResponse(data.choices[0].message.content);
-                setLoaoding(false);
-        }
-        fetchExplanation();
-        
+        setResponse(getExplanation());
+        setLoaoding(false);
 
     }, [word]);
     
